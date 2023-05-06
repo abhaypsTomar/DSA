8fdbd3f38e0cbe8202239f1d5a986d30087c832d
abhaypsTomar <abhaypratap20315@gmail.com>
2023-05-06 17:16:51 +0530

    modified:   codes/asc_dsc.java new file:   codes/ceilingNumber.java modified:   codes/counting_occurance.java new file:   codes/cyclicsort.java new file:   codes/infiniteSearch.java

--- codes/asc_dsc.java
+++ codes/asc_dsc.java
@@ -1,14 +1,52 @@
-//import java.util.Scanner;
-//public class asc_dsc {
-//    public static void main(String[] args){
-//        Scanner in = new Scanner(System.in);
-//        int n = in.nextInt();
-//        int[] arr = new int[n];
-//        for(int i = 0;i<arr.length;i++){
-//            arr[i]=in.nextInt();
-//        }
-//        int target = in.nextInt();
-//    }
-//    static int binaray_search(int[] arr , int target){
-//
-////    }
+import java.util.Arrays;
+import java.util.Scanner;
+public class asc_dsc {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<arr.length;i++){
+            arr[i]=in.nextInt();
+        }
+        int target = in.nextInt();
+        int ans = binaray_search(arr, target);
+        System.out.println(ans);
+    }
+    /**
+     * @param arr
+     * @param target
+     * @return
+     */
+    static int binaray_search(int[] arr , int target){
+        int start =0;
+        int end = arr.length -1;
+
+
+        boolean isAsc=arr[start]<arr[end];
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(target == mid){
+                return mid;
+            }
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            else
+            {
+                    if(target>arr[mid]){
+                        end = mid -1;
+                    }else {
+                        start = mid+1;
+                    }
+                }
+        }
+        return -1;
+
+
+    }
+}
---
+++ codes/ceilingNumber.java
@@ -0,0 +1,31 @@
+// public class ceilingNumber{
+//     /**
+//      * @param args
+//      */
+//     public static void  main(String[] args){
+//         int[] arr ={2,3,5,9,14,16,18};
+//         int target = 15;
+//         int ans = binary(arr,target);
+//         System.out.println(target);
+
+//     static int binary(int[] arr,int target){
+//         int start = 0;
+//         int end = arr.length-1;
+//         while(start<=end){
+//             int mid = start+(end-start)/2;
+//             if(arr[mid]<target){
+//                 start = mid+1;
+//             }
+//             else if(arr[mid]>target){
+//                 end = mid-1;
+//             }
+//             else {
+//                 return mid;
+//             }
+            
+//         }
+//       return start;
+     
+//     }
+// }
+// }
